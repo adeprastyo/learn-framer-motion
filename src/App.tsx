1a3abb2daa70164765e21b9fa1b49349ad1ae45d
adeprastyo <adeprastyo101@gmail.com>
2024-06-28 11:08:26 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-xl font-bold">
+          React vite + tailwindcss + typescript Template
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
